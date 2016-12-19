14547f71bbc5d20fde790ab2722c8dce293fc618
eyu <eyuel.belay@gmail.com>
2016-12-19 17:23:29 +0100

    modified from eclipse

--- muk/src/algo/IRecursive.java
+++ muk/src/algo/IRecursive.java
@@ -1,10 +1,15 @@
 package algo;
 
 public interface IRecursive {
-  int factorial(int number);
-  int power(int base, int exponent);
-  int sum(int number);
-  int sum(int number[]);
-  int fibonacci(int number);
-  
+	
+	int factorial(int number);
+
+	int power(int base, int exponent);
+
+	int sum(int number);
+
+	int sum(int number[]);
+
+	int fibonacci(int number);
+
 }
